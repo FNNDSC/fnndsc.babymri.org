070cee5cd44e60dd3796cdb986c5e7a9c1f1e1d6
nicolasrannou <npm.rannou@gmail.com>
2016-03-16 08:59:42 +0100

    use PSK

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,287 +1,81 @@
-(function (document) {
+/*
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+(function(document) {
   'use strict';
 
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#fnndscbabymriorg');
-  app.logo = 'images/images/logo.png';
-  app.label = 'fnndsc.babymri.org';
-  // 500 100 700
-  // 200 100 400
-  app.chapters = [
-            {id: 'welcome', label: 'Welcome',
-            style: {
-              primary: {
-                normal : {
-                  color: '#3F51B5',
-                  font: '#ffffff'},
-                light : {
-                  color: '#C5CAE9',
-                  font: '#ffffff'},
-                dark : {
-                  color: '#303F9F',
-                  font: '#ffffff'},
-                bw : {
-                  color: '#212121',
-                  font: '#f9f9f9'},
-                wb : {
-                  color: '#f9f9f9',
-                  font: '#212121'}
-                },
-              accent: {
-                normal : {
-                  color: '#E91E63',
-                  font: '#ffffff'},
-                light : {
-                  color: '#F8BBD0',
-                  font: '#000000'},
-                dark : {
-                  color: '#C2185B',
-                  font: '#ffffff'},
-                bw : {
-                  color: '#212121',
-                  font: '#f9f9f9'},
-                wb : {
-                  color: '#f9f9f9',
-                  font: '#212121'}
-                }
-              },
-            pages: [
-              {id:'home',
-              label: 'Home',
-              element: 'home-element',
-              key: '1ZIXvH4lmPGYzGkVxbfBvr-kc6dbIiqwbs95VcjqvzWY'
-            },
-            {id:'challenges',
-              label: 'Challenges',
-              element: 'home-element',
-              key: '1UDJ_SgF744k_xK90iISx7Gxt_-luxD7W8bvVB-f6jW8'
-            }
-            ]},
-            {id: 'mri', label: 'Magnetic resonance imaging',
-            style: {
-              primary: {
-                normal : {
-                  color: '#E91E63',
-                  font: '#ffffff'},
-                light : {
-                  color: '#F8BBD0',
-                  font: '#000000'},
-                dark : {
-                  color: '#C2185B',
-                  font: '#ffffff'},
-                bw : {
-                  color: '#212121',
-                  font: '#f9f9f9'},
-                wb : {
-                  color: '#f9f9f9',
-                  font: '#212121'}
-                },
-              accent: {
-                normal : {
-                  color: '#69F0AE',
-                  font: '#000000'},
-                light : {
-                  color: '#B9F6CA',
-                  font: '#000000'},
-                dark: {
-                  color: '#00E676',
-                  font: '#ffffff'},
-                bw : {
-                  color: '#212121',
-                  font: '#f9f9f9'},
-                wb : {
-                  color: '#f9f9f9',
-                  font: '#212121'}
-                }
-              },
-            pages: [
-              {id:'introduction',
-              label: 'Introduction',
-              element: 'home-element',
-              key: '1_DbTw4bsj82m6aVLm-Q3ujKZJ5z678s0Jm6pdlp--BM'
-            },
-              {id:'members',
-              label: 'Members',
-              element: 'members-element',
-              key: '1SthCokDqdHWO7coUO1yT2dcT0ioOJ1RGJ9po5vbueNs'
-            },
-              {id:'contact',
-              label: 'Contact',
-              element: 'contact-element',
-              key: '1LgZmITZQA9UkxCSPLTB4aO1iXw4pASruUuTtFRAsX0o'
-            }
-            ]},
-            {id: 'meg', label: 'Magnetoencephalography',
-            style: {
-              primary: {
-                normal : {
-                  color: '#8BC34A',
-                  font: '#ffffff'},
-                light : {
-                  color: '#DCEDC8',
-                  font: '#000000'},
-                dark : {
-                  color: '#689F38',
-                  font: '#000000'},
-                bw : {
-                  color: '#212121',
-                  font: '#f9f9f9'},
-                wb : {
-                  color: '#f9f9f9',
-                  font: '#212121'}
-                },
-              accent: {
-                normal : {
-                  color: '#E040FB',
-                  font: '#ffffff'},
-                light : {
-                  color: '#EA80FC',
-                  font: '#ffffff'},
-                dark : {
-                  color: '#D500F9',
-                  font: '#ffffff'},
-                bw : {
-                  color: '#212121',
-                  font: '#f9f9f9'},
-                wb : {
-                  color: '#f9f9f9',
-                  font: '#212121'}
-                }
-              },
-            pages: [
-              {id:'introduction',
-              label: 'Introduction',
-              element: 'home-element',
-              key: '15WlhjU0mMmQb6Ln1Bghbjn9_0BirOBwjbYSDWf2zo70'
-            },
-              {id:'members',
-              label: 'Members',
-              element: 'members-element',
-              key: '18wLiTBCiVIOE-ByELf4Ov0H-r1I18T_2bl5fMzBQifo'
-            },
-              {id:'contact',
-              label: 'Contact',
-              element: 'contact-element',
-              key: '1ZPliddJ9NietZa4IkJeQSKYGW4CS83Y_JnwMSaW3_Hk'
-            }
-            ]},
-            {id: 'nirs', label: 'Near-infrared spectroscopy',
-            style: {
-              primary: {
-                normal : {
-                  color: '#673AB7',
-                  font: '#ffffff'},
-                light : {
-                  color: '#D1C4E9',
-                  font: '#000000'},
-                dark : {
-                  color: '#512DA8',
-                  font: '#000000'},
-                bw : {
-                  color: '#212121',
-                  font: '#f9f9f9'},
-                wb : {
-                  color: '#f9f9f9',
-                  font: '#212121'}
-                },
-              accent: {
-                normal : {
-                  color: '#FF6E40',
-                  font: '#000000'},
-                light : {
-                  color: '#FF9E80',
-                  font: '#000000'},
-                dark : {
-                  color: '#FF3D00',
-                  font: '#ffffff'},
-                bw : {
-                  color: '#212121',
-                  font: '#f9f9f9'},
-                wb : {
-                  color: '#f9f9f9',
-                  font: '#212121'}
-                }
-              },
-            pages: [
-              {id:'introduction',
-              label: 'Introduction',
-              element: 'home-element',
-              key: '1TEFMDODoWJimTCHV9ZqogUwSDQkav-2eegzJmSubA_s'
-            },
-              {id:'members',
-              label: 'Members',
-              element: 'members-element',
-              key: '1oZQ9ZP6R7o1_3WwWkirr7Gs4EXjgODRW-OMLNN3pzjg'
-            },
-              {id:'contact',
-              label: 'Contact',
-              element: 'contact-element',
-              key: '16RIvt08KxYyJdpO9nApZhjD2LTm23uM5HktrVwsMa0Y'
-            }
-            ]},
-            {id: 'softwares', label: 'Softwares',
-            style: {
-              primary: {
-                normal : {
-                  color: '#607D8B',
-                  font: '#ffffff'},
-                light : {
-                  color: '#CFD8DC',
-                  font: '#000000'},
-                dark : {
-                  color: '#455A64',
-                  font: '#000000'},
-                bw : {
-                  color: '#212121',
-                  font: '#f9f9f9'},
-                wb : {
-                  color: '#f9f9f9',
-                  font: '#212121'}
-                },
-              accent: {
-                normal : {
-                  color: '#64FFDA',
-                  font: '#000000'},
-                light : {
-                  color: '#A7FFEB',
-                  font: '#000000'},
-                dark : {
-                  color: '#1DE9B6',
-                  font: '#ffffff'},
-                bw : {
-                  color: '#212121',
-                  font: '#f9f9f9'},
-                wb : {
-                  color: '#f9f9f9',
-                  font: '#212121'}
-                }
-              },
-            pages:[
-              {id:'introduction',
-              label: 'Introduction',
-              element: 'home-element',
-              key: '1q6BcByK7665nA9Eyn7BYGzudYfXX9knA37W8IymDUZs'
-            },
-              {id:'members',
-              label: 'Members',
-              element: 'members-element',
-              key: '1RTgmAK-KCsXmf_a-W5CNT9oykyrMR0KbUoL6wBjZhD4'
-            },
-              {id:'contact',
-              label: 'Contact',
-              element: 'contact-element',
-              key: '1C3ri1r_6k1Z1pZYKF2IOwuI1kRs82F1zh_zcZLOudlE'
-            }
-            ]},];
+  var app = document.querySelector('#app');
+  
+  // Sets app default base URL
+  app.baseUrl = '/';
+  if (window.location.port === '') {  // if production
+    // Uncomment app.baseURL below and
+    // set app.baseURL to '/your-pathname/' if running from folder in production
+    // app.baseUrl = '/polymer-starter-kit/';
+  }
+
+  app.displayInstalledToast = function() {
+    // Check to make sure caching is actually enabled—it won't be in the dev environment.
+    if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
+      Polymer.dom(document).querySelector('#caching-complete').show();
+    }
+  };
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  // app.addEventListener('template-bound', function() {
-  //   console.log('Our app is ready to rock!');
-  // });
+  app.addEventListener('dom-change', function() {
+    console.log('Our app is ready to rock!');
+  });
+  
+  // See https://github.com/Polymer/polymer/issues/1381
+  window.addEventListener('WebComponentsReady', function() {
+    // imports are loaded and elements have been registered
+  });
 
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
-})(wrap(document));
+  // Main area's paper-scroll-header-panel custom condensing transformation of
+  // the appName in the middle-container and the bottom title in the bottom-container.
+  // The appName is moved to top and shrunk on condensing. The bottom sub title
+  // is shrunk to nothing on condensing.
+  window.addEventListener('paper-header-transform', function(e) {
+    var appName = Polymer.dom(document).querySelector('#mainToolbar .app-name');
+    var middleContainer = Polymer.dom(document).querySelector('#mainToolbar .middle-container');
+    var bottomContainer = Polymer.dom(document).querySelector('#mainToolbar .bottom-container');
+    var detail = e.detail;
+    var heightDiff = detail.height - detail.condensedHeight;
+    var yRatio = Math.min(1, detail.y / heightDiff);
+    // appName max size when condensed. The smaller the number the smaller the condensed size.
+    var maxMiddleScale = 0.50;
+    var auxHeight = heightDiff - detail.y;
+    var auxScale = heightDiff / (1 - maxMiddleScale);
+    var scaleMiddle = Math.max(maxMiddleScale, auxHeight / auxScale + maxMiddleScale);
+    var scaleBottom = 1 - yRatio;
+
+    // Move/translate middleContainer
+    Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
+
+    // Scale bottomContainer and bottom sub title to nothing and back
+    Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
+
+    // Scale middleContainer appName
+    Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
+  });
+
+  // Scroll page to top and expand header
+  app.scrollPageToTop = function() {
+    app.$.headerPanelMain.scrollToTop(true);
+  };
+
+  app.closeDrawer = function() {
+    app.$.paperDrawerPanel.closeDrawer();
+  };
+
+})(document);
